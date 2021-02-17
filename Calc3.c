85b30fa1830c5bc9865cbfd8d12c4d9d9fbb65be
FezuPapa <79169429+FezuPapa@users.noreply.github.com>
2021-02-18 00:16:33 +0100

    Add files via upload

---
+++ Calc3.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+  //set value and operator
+  int num1, num2;
+  int loop = 1;
+  char oper;
+
+
+  while(loop)
+  {
+
+    printf("Enter a value: ");
+    scanf("%d", &num1);
+
+    printf("Enter an operator: ");
+    scanf(" %c", &oper);
+
+    printf("Enter a value: ");
+    scanf("%d", &num2);
+
+       switch(oper)
+       {
+         case '+':
+         printf("= %d\n\n", num1 + num2);
+         break;
+
+         case '-':
+         printf("= %d\n\n", num1 - num2);
+         break;
+
+         case '*':
+         printf("= %d\n\n", num1 * num2);
+         break;
+
+         case '/':
+         printf("= %d\n\n", num1 / num2);
+         break;
+        }
+
+            char userinput;;
+            scanf("%c", &userinput);
+
+            printf("Y/N Would you like to continue ?\n\n");
+            scanf("%c", &userinput);
+
+            if (userinput == 'y')
+            {
+                loop = 1;
+            }
+
+   }
+
+
+
+  return 0;
+}
